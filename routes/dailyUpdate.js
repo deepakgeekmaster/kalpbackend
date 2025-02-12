152ea12b08a3efacd76b9dbeb055817ef396eacd
deepakgeekmaster <deepakgeekmaster@gmail.com>
2025-02-12 16:41:36 +0530

    Update dailyUpdate.js

--- routes/dailyUpdate.js
+++ routes/dailyUpdate.js
@@ -4,14 +4,22 @@ const multer = require('multer');
 const path = require('path');
 const dailyupdateController = require('../controllers/dailyupdateController');
 const FormData = require("form-data");
-
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = "/tmp"; // Use /tmp since Vercel is read-only except this directory
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
 const upload = multer({ storage });
 
 router.post('/saveData', upload.single('name'), dailyupdateController.saveData);  // 'name' is the file input name
